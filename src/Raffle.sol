3958c53f050deb8f2e036f922f48455a2ae599dc
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-10-13 11:05:36 +0200

    Custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,19 +8,27 @@ pragma solidity 0.8.19;
  * @dev Implement Chainlink VRF to generate random numbers
  */
 contract Raffle {
+    /*       ERRORS       */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");          // Version 1: Expensive way to print an error message
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());        // Version 3 - from 0.8.26: Cheaper way to print an error message, readable
+        // Version 2: Cheaper way to print an error message, not really readable but it's ok for 0.8.19
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
 
-    /**
-     * GETTER FUNCTIONS
-     */
+    /*       GETTER FUNCTIONS       */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
