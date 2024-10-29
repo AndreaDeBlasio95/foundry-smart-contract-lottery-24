d5b1f3e5b229dcc0a7dbacea482ed2555ad0ca4f
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-10-29 09:25:15 +0100

    Start working on testing

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -45,6 +45,10 @@ contract HelperConfig is Script, CodeConstants {
         }
     }
 
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({
             entranceFee: 0.1 ether,
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -41,7 +41,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
     address private s_recentWinner;
-    RaffleState private s_raffleState;
+    RaffleState private s_raffleState; // start as open
 
     constructor(
         uint256 entranceFee,
@@ -154,5 +154,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
     /*       GETTER FUNCTIONS       */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getRaffleState() external view returns (RaffleState) {
+        return s_raffleState;
     }
 }
